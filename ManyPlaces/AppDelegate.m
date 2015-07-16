605cc57c05dbe0ba09110a0b8be1c9761010efe5
Tin Nguyen <tinmnguyen@gmail.com>
2015-07-16 16:41:43 -0700

    Add DCintrospect pod

--- ManyPlaces/AppDelegate.m
+++ ManyPlaces/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <DCIntrospect.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+#if TARGET_IPHONE_SIMULATOR
+    [[DCIntrospect sharedIntrospector] start];
+#endif
     return YES;
 }
 
--- Podfile
+++ Podfile
@@ -3,9 +3,12 @@ platform :ios, '8.0'
 
 source 'https://github.com/CocoaPods/Specs.git'
 
+inhibit_all_warnings!
+
 target 'ManyPlaces' do
 pod 'GoogleMaps'
 pod 'Motis'
+pod 'DCIntrospect-ARC'
 end
 
 target 'ManyPlacesTests' do
